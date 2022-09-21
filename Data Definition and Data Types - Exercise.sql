ffdf721a0be85597dda87d854b3cd066462d4544
Nadya Salem <nadya_salem@abv.bg>
2022-09-21 15:18:24 +0300

    Update Data Definition and Data Types - Exercise.sql

--- Data Definition and Data Types - Exercise.sql
+++ Data Definition and Data Types - Exercise.sql
@@ -314,3 +314,17 @@ VALUES
 SELECT * FROM `towns`;
 SELECT * FROM `departments`;
 SELECT * FROM `employees`;
+
+  -- 15. Basic Select All Fields and Order Them
+  
+  SELECT * FROM `towns`
+ORDER BY `name`;
+
+SELECT * FROM `departments`
+ORDER BY `name`;
+
+SELECT * FROM `employees`
+ORDER BY `salary` DESC;
+
+
+
