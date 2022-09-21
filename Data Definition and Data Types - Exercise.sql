0006ed4b263a6a2f868167ab86bfa8be95d3c40d
Nadya Salem <nadya_salem@abv.bg>
2022-09-21 15:05:55 +0300

    Update Data Definition and Data Types - Exercise.sql

--- Data Definition and Data Types - Exercise.sql
+++ Data Definition and Data Types - Exercise.sql
@@ -243,3 +243,68 @@ CREATE TABLE `rental_orders` (
 
  INSERT INTO  `rental_orders` (`employee_id`)
  VALUES (001), ('002'), ('003');
+
+
+ -- 13. Basic Insert
+ 
+ CREATE DATABASE `soft_uni`;
+
+USE `soft_uni`;
+
+CREATE TABLE `towns` (
+`id` INT PRIMARY KEY AUTO_INCREMENT, 
+`name` VARCHAR (20)
+);
+
+CREATE TABLE `addresses` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `address_text` VARCHAR(70),
+    `town_id` INT,
+    FOREIGN KEY (`town_id`)
+	REFERENCES `towns` (`id`)
+);
+ 
+ CREATE TABLE `departments` (
+`id` INT PRIMARY KEY AUTO_INCREMENT, 
+`name` VARCHAR (50)
+);
+
+CREATE TABLE `employees` (
+`id`INT PRIMARY KEY AUTO_INCREMENT,
+`first_name`VARCHAR (50) NOT NULL,
+`middle_name`VARCHAR (50),
+`last_name`VARCHAR (50) NOT NULL,
+`job_title`VARCHAR (50) NOT NULL,
+`department_id` INT,
+`hire_date` DATE,
+`salary` DECIMAL (7,3),
+`address_id` INT,
+FOREIGN KEY (`department_id`)
+	REFERENCES `departments` (`id`),
+FOREIGN KEY (`address_id`)
+	REFERENCES `addresses` (`id`)
+);
+
+INSERT INTO `towns`(`name`)
+VALUES
+	('Sofia'), 
+    ('Plovdiv'), 
+    ('Varna'), 
+    ('Burgas');
+    
+INSERT INTO `departments`(`name`)
+	VALUES
+		('Engineering'), 
+        ('Sales'), 
+        ('Marketing'),
+        ('Software Development'),
+        ('Quality Assurance'); 
+
+INSERT INTO `employees` (`first_name`, `middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`)
+VALUES
+	('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),        
+	('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),        
+	('Maria', 'Petrova', 'Ivanova',	'Intern', 5, '2016-08-28', 525.25),        
+	('Georgi', 'Terziev', 'Ivanov',	'CEO', 2, '2007-12-09',	3000.00),        
+	('Peter', 'Pan', 'Pan',	'Intern', 3, '2016-08-28', 599.88);
+        
