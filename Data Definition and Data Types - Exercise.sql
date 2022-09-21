0d34b80a3aff00788f7bf72f61dbb63346649693
Nadya Salem <nadya_salem@abv.bg>
2022-09-21 14:05:36 +0300

    Update Data Definition and Data Types - Exercise.sql

--- Data Definition and Data Types - Exercise.sql
+++ Data Definition and Data Types - Exercise.sql
@@ -162,3 +162,84 @@ CREATE TABLE `movies` (
 
 INSERT INTO  `movies` (`title`)
 VALUES ('title1'), ('title2'), ('title3'), ('title4'), ('title5');
+
+
+ -- 12. Car Rental Database
+
+CREATE DATABASE `car_rental`;
+
+USE `car_rental`;
+
+CREATE TABLE `categories` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `category` VARCHAR(30) NOT NULL,
+    daily_rate DOUBLE,
+    weekly_rate DOUBLE,
+    monthly_rate DOUBLE,
+    weekend_rate DOUBLE
+);
+
+INSERT INTO `categories` (`category`)
+VALUES ('category1'), ('category2'), ('category3');
+
+CREATE TABLE `cars` (
+ `id` INT PRIMARY KEY AUTO_INCREMENT,
+ `plate_number` VARCHAR (20),
+ `make` VARCHAR (20),
+ `model` VARCHAR (30),
+ `car_year` INT,
+ `category_id` INT,
+ `doors` INT,
+ `picture` BLOB,
+ `car_condition` VARCHAR (50),
+ `available` BOOLEAN
+ );
+ 
+INSERT INTO `cars` (`model`)
+VALUES ('model1'), ('model2'), ('model3');
+
+CREATE TABLE `employees` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `first_name` VARCHAR(30),
+    `last_name` VARCHAR(30),
+    `title` VARCHAR(50),
+    `notes` TEXT
+);
+
+INSERT INTO  `employees` (`first_name`)
+VALUES ('Nadya'), ('Lora'), ('Lina');
+
+CREATE TABLE `customers` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`driver_licence_number` VARCHAR(30),
+ `full_name`VARCHAR(50),
+ `address` VARCHAR(70),
+ `city` VARCHAR(20),
+ `zip_code` VARCHAR(20),
+ `notes` TEXT
+ );
+ 
+ INSERT INTO  `customers` (`city`)
+ VALUES ('Kavarna'), ('Varna'), ('Shumen');
+
+CREATE TABLE `rental_orders` (
+   `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `employee_id` INT,
+    `customer_id` INT,
+    `car_id` INT,
+    `car_condition` VARCHAR(70),
+    `tank_level` VARCHAR(30),
+    `kilometrage_start` INT,
+    `kilometrage_end` INT,
+    `total_kilometrage` INT,
+    `start_date` DATE, 
+    `end_date` DATE, 
+    `total_days` INT,
+    `rate_applied` DOUBLE,
+    `tax_rate` DOUBLE,
+    `order_status` VARCHAR(40),
+    `notes` TEXT
+ );
+
+ INSERT INTO  `rental_orders` (`employee_id`)
+ VALUES (001), ('002'), ('003');
